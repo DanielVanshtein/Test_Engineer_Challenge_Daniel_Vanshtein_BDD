107b711ba12f38cb3a5105e8a284246fa40fb7ba
B21_Daniel_Vanshtein <vanshtein@gmail.com>
2021-05-01 11:18:26 -0400

    Added a FailedRunner.java class

--- src/test/java/com/assignment_Test/runners/CukesRunner.java
+++ src/test/java/com/assignment_Test/runners/CukesRunner.java
@@ -7,8 +7,10 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin = {"json:target/cucumber.json",
-                "rerun:target/rerun.txt"},
+        plugin = {
+                "json:target/cucumber.json",
+                "rerun:target/rerun.txt"
+        },
         features = "src/test/resources/features",
         glue = "com/assignment_Test/step_definitions",
         dryRun = false,
